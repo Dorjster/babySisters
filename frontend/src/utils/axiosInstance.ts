c72fbdf4c03d029e93521de2ca16c109e03a33c2
Dorjster <thebuyndorj@gmail.com>
2024-03-29 09:38:20 +0800

    deploymentBackend

--- frontend/src/utils/axiosInstance.ts
+++ frontend/src/utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const AxiosInstance = axios.create({
-  baseURL: "https://teamproject-babysits.onrender.com",
+  baseURL: "https://babysisters.onrender.com",
   headers: {
     Authorization: "Bearer token",
   },
