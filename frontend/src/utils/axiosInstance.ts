ab6491938d1951cdc50c857c12028c8aa9387194
Dorjster <thebuyndorj@gmail.com>
2024-04-02 09:36:08 +0800

    login

--- frontend/src/utils/axiosInstance.ts
+++ frontend/src/utils/axiosInstance.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const AxiosInstance = axios.create({
-  baseURL: "https://babysisters.onrender.com",
+  // baseURL: "https://babysisters.onrender.com",
+  baseURL: "http://localhost:8000",
   headers: {
     Authorization: "Bearer token",
   },
