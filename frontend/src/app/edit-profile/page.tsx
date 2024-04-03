86b2dc481800ed60afb084dee465f50f354cee99
Dorjster <thebuyndorj@gmail.com>
2024-04-03 13:29:29 +0800

    editPro1

--- frontend/src/app/edit-profile/page.tsx
+++ frontend/src/app/edit-profile/page.tsx
@@ -1,12 +1,10 @@
+import { Profiles } from "@/components/editParentProfile/Profiles";
 import React from "react";
-
-import { EditParent } from "@/components/editParentProfile/EditParent";
-import { EditBabysitProfile } from "@/components/editBabySitProfile/EditBabysitProfile";
 
 const page = () => {
   return (
     <div>
-      <EditParent />
+      <Profiles />
     </div>
   );
 };
