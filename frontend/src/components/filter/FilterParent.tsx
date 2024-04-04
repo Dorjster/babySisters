2a68112ac49f00734e4fec682967bebb62dea875
binderiyabilguun@gmail.com <binderiyabilguun@gmail.com>
2024-04-04 12:28:44 +0800

    cloudflare

--- frontend/src/components/filter/FilterParent.tsx
+++ frontend/src/components/filter/FilterParent.tsx
@@ -6,57 +6,58 @@ import { Wage } from "./Wage";
 import { useState } from "react";
 import { MdVerified } from "react-icons/md";
 
-
 export const FilterParent = () => {
-    const [sliderValue, setSliderValue] = useState<number>(2);
+  const [sliderValue, setSliderValue] = useState<number>(2);
 
-    const handleSliderChange = (value: number | number[]) => {
-        setSliderValue(value as number);
-    };
+  const handleSliderChange = (value: number | number[]) => {
+    setSliderValue(value as number);
+  };
 
-    const [sliderRatingValue, setSliderRatingValue] = useState<number>(2);
+  const [sliderRatingValue, setSliderRatingValue] = useState<number>(2);
 
-    const handleSliderRatingChange = (value: number | number[]) => {
-        setSliderRatingValue(value as number);
-    };
+  const handleSliderRatingChange = (value: number | number[]) => {
+    setSliderRatingValue(value as number);
+  };
 
-    return (
-        <div className="w-[300px]  bg-[#f5f5f5] p-10 grid gap-5 rounded-xl h-fit sticky top-[20%]">
-            <div className="grid gap-3">
-                <p className="text-m font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-                    Цалин
-                </p>
-                <Wage />
-            </div>
+  return (
+    <div className="w-[300px]  bg-slate-50 p-10 grid gap-5 rounded-xl h-fit sticky top-[20%]">
+      <div className="grid gap-3">
+        <p className="text-m font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+          Цалин
+        </p>
+        <Wage />
+      </div>
 
-            <Separator />
+      <Separator />
 
-            <div className="grid gap-3">
-                <p className="text-m font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-                    Байршил
-                </p>
-                <LocationSelect />
-            </div>
+      <div className="grid gap-3">
+        <p className="text-m font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+          Байршил
+        </p>
+        <LocationSelect />
+      </div>
 
-            <Separator />
+      <Separator />
 
-            <div className="grid gap-3">
-                <p className="text-m font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-                    Нэмэлт
-                </p>
-                <div className="flex gap-3">
-                    <div className="flex items-center space-x-2">
-                        <Checkbox/>
-                    </div>
-                    <div className="flex gap-2">
-                        <p className="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Баталгаажсан</p>
-                        <MdVerified/>
-                    </div>
-                </div>
-            </div>
-            <button className="bg-[#c9e8ec] cursor-pointer rounded-[20px] py-2 sticky bottom-3">
-                Button{" "}
-            </button>
+      <div className="grid gap-3">
+        <p className="text-m font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+          Нэмэлт
+        </p>
+        <div className="flex gap-3">
+          <div className="flex items-center space-x-2">
+            <Checkbox className="rounded-[4px]" />
+          </div>
+          <div className="flex gap-2">
+            <p className="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+              Баталгаажсан
+            </p>
+            <MdVerified />
+          </div>
         </div>
-    );
+      </div>
+      <button className="bg-[#389BA7] cursor-pointer text-white rounded-[20px] py-2 sticky bottom-3">
+        Хайх{" "}
+      </button>
+    </div>
+  );
 };
--- frontend/src/components/filter/Info.tsx
+++ frontend/src/components/filter/Info.tsx
@@ -1,54 +1,56 @@
-import { FaTransgender, FaAddressCard, FaBaby, FaCar, FaUserGraduate } from "react-icons/fa";
+import {
+  FaTransgender,
+  FaAddressCard,
+  FaBaby,
+  FaCar,
+  FaUserGraduate,
+} from "react-icons/fa";
 import { MdVerified, MdOutlineSmokeFree } from "react-icons/md";
 import { Checkbox } from "../ui";
 
-
 export const Info = () => {
-
-    const info = [
-        {
-            icon: <MdVerified />,
-            id: "verified",
-            text: "Баталгаажсан"
-        },
-        {
-            icon: <FaAddressCard />,
-            id: "driver",
-            text: "Жолооны үнэмлэхтэй"
-        },
-        {
-            icon: <FaCar />,
-            id: "hasCar",
-            text: "Машинтай"
-        },
-        {
-            icon: <FaBaby />,
-            id: "hasChildren",
-            text: "Хүүхэдтэй"
-        },
-        {
-            icon: <MdOutlineSmokeFree />,
-            id: "nonSmoker",
-            text: "Тамхи татдаггүй"
-        }
-    ]
-    return (
-        <>
-        {info.map((el, index) => (
-
-            <div key={index} className="flex gap-3">
-                <div className="flex items-center space-x-2">
-                    <Checkbox id={el.id} />
-                </div>
-                <div className="flex gap-2">
-                    <p className="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">{el.text}</p>
-                    {el.icon}
-                    
-                </div>
-            </div>
-        ))}
-           
-        </>
-
-    )
-}
+  const info = [
+    {
+      icon: <MdVerified />,
+      id: "verified",
+      text: "Баталгаажсан",
+    },
+    {
+      icon: <FaAddressCard />,
+      id: "driver",
+      text: "Жолооны үнэмлэхтэй",
+    },
+    {
+      icon: <FaCar />,
+      id: "hasCar",
+      text: "Машинтай",
+    },
+    {
+      icon: <FaBaby />,
+      id: "hasChildren",
+      text: "Хүүхэдтэй",
+    },
+    {
+      icon: <MdOutlineSmokeFree />,
+      id: "nonSmoker",
+      text: "Тамхи татдаггүй",
+    },
+  ];
+  return (
+    <>
+      {info.map((el, index) => (
+        <div key={index} className="flex gap-3">
+          <div className="flex items-center space-x-2">
+            <Checkbox className="rounded-[4px]" id={el.id} />
+          </div>
+          <div className="flex gap-2">
+            <p className="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+              {el.text}
+            </p>
+            {el.icon}
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
