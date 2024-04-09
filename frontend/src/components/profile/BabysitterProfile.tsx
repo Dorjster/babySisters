d290ea96fc134791b00f1d1fd0e96499bb011727
Dorjster <thebuyndorj@gmail.com>
2024-04-09 08:31:50 +0800

    profileBugFIxed

--- frontend/src/components/profile/BabysitterProfile.tsx
+++ frontend/src/components/profile/BabysitterProfile.tsx
@@ -25,31 +25,34 @@ import babysitter from "@/app/babysitter/page";
 import { CheckedSchedule } from "./CheckedSchedule";
 
 type All = {
-  result: ProfileType;
+  result: ProfileType[] & any;
 };
 
 export const BabysitterProfile = (props: All) => {
   const { result } = props;
 
-  useEffect(() => {
-    const token: string | null = localStorage.getItem("token");
-  });
+  const info = result[0].info_id[0];
+  console.log(info);
+
+  // useEffect(() => {
+  //   const token: string | null = localStorage.getItem("token");
+  // });
 
   return (
     <div className="bg-gradient-to-b m-auto   h-fit flex justify-start py-32 px-[400px]">
       <div className="w-[60%]">
         <div className="flex border-b-[0.5px] flex-col gap-4 border-gray-600 pb-[40px]">
           <p className="text-[28px] text-gray-900 ">Миний тухай</p>
-          <p className="overflow-wrap break-word">{result?.about}</p>
+          <p className="overflow-wrap break-word">{result[0]?.about}</p>
           <div className="flex gap-6 pt-4">
             <div className="flex gap-8">
               <div className="flex items-center gap-1 text-[18px] ">
                 <MdBabyChangingStation className="text-[#008291]" size={24} />
-                {/* <div>{result?.info_id.year_of_experience} жил</div> */}
+                <div>{info?.year_of_experience} жил</div>
               </div>
               <div className="flex items-center gap-1 text-[18px]">
                 <FaTransgender className="text-[#008291]" size={24} />
-                {result?.gender === true ? (
+                {result[0]?.gender === true ? (
                   <div>Эмэгтэй</div>
                 ) : (
                   <div>Эрэгтэй</div>
@@ -57,11 +60,11 @@ export const BabysitterProfile = (props: All) => {
               </div>
               <div className="flex items-center gap-1 text-[18px]">
                 <MdLocationOn className="text-[#008291]" size={24} />
-                {result?.address}
+                {result[0]?.address}
               </div>
               <div className="flex items-center gap-1 text-[18px]">
                 <FaUserGraduate className="text-[#008291] ]" size={22} />
-                {/* {result?.info_id.education} */}
+                {info?.education}
               </div>
             </div>
             <div></div>
@@ -71,27 +74,27 @@ export const BabysitterProfile = (props: All) => {
           <div>
             <h1 className="text-[23px] pb-5 ml-[20px]">Ур чадвар</h1>
             <div className=" rounded-2xl px-8 py-4 flex w-[400px] gap-[30px] text-[18px] flex-wrap">
-              {/* {result?.info_id.skills?.map((el, index) => (
+              {info?.skills?.map((el: string, index: number) => (
                 <div
                   className="flex gap-2 bg-[#c9e8ec] shadow-md shadow-[#c5c5c5] w-fit rounded-xl px-4 py-2 text-gray-700 text-[16px]"
                   key={index}
                 >
                   {el}
                 </div>
-              ))} */}
+              ))}
             </div>
           </div>
           <div>
             <h1 className="text-[23px] pb-5 ml-[20px]">Зан чанар</h1>
             <div className=" rounded-2xl px-8 py-4 flex w-[400px] gap-[30px] text-[18px] flex-wrap">
-              {/* {result?.info_id.character?.map((el, index) => (
+              {info?.character?.map((el: string, index: number) => (
                 <div
                   className="flex gap-2 bg-[#c9e8ec] shadow-md shadow-[#c5c5c5] w-fit rounded-xl px-4 py-2 text-[16px] text-gray-700"
                   key={index}
                 >
                   {el}
                 </div>
-              ))} */}
+              ))}
             </div>
           </div>
         </div>
@@ -233,43 +236,41 @@ export const BabysitterProfile = (props: All) => {
         </div>
         <div className="text-[18px]">
           <div className="flex items-center justify-center gap-4 ">
-            <h1 className="text-[26px]">{result?.name}</h1>
-            {result?.verification && (
+            <h1 className="text-[26px]">{result[0]?.name}</h1>
+            {result[0]?.verification && (
               <MdVerified className="text-[#008291]" size={25} />
             )}
           </div>
           <div className="flex flex-col justify-center items-center">
             <div>
-              {/* {result?.info_id.rating && (
+              {info?.rating && (
                 <Rating
                   // sx={{ color: "#59BEC9" }}
                   name="read-only"
-                  defaultValue={result.info_id.rating}
+                  defaultValue={info?.rating}
                   readOnly
                 />
-              )} */}
+              )}
             </div>
           </div>
           <div className="flex gap-4 bg-[#edf7f8] items-center justify-center rounded-2xl py-2 px-4 mt-[20px]">
-            {/* {result?.info_id.smoker === false && (
+            {info?.smoker === false && (
               <MdOutlineSmokeFree className="h-6 w-6 text-[#008291]" />
             )}
-            {result?.info_id.car === true && (
-              <FaCar className="h-6 w-6 text-[#008291]" />
-            )}
-            {result?.info_id.driver_license === true && (
+            {info?.car === true && <FaCar className="h-6 w-6 text-[#008291]" />}
+            {info?.driver_license === true && (
               <FaAddressCard className="h-6 w-6 text-[#008291]" />
             )}
-            {result?.info_id.has_children === true && (
+            {info?.has_children === true && (
               <FaChild className="h-6 w-6 text-[#008291]" />
-            )} */}
+            )}
           </div>
         </div>
 
         <div className="bg-[#edf7f8] w-fill flex flex-col gap-3 rounded-2xl px-2 py-4 sticky top-24 ">
           <h1 className="text-[20px] flex gap-1 items-center justify-center">
             <TbCurrencyTugrik />
-            {/* {result?.info_id.wage}/цагт */}
+            {info?.wage}/цагт
           </h1>
           <button className="text-white bg-[#008291] rounded-[20px] w-[200px] px-6 py-1 mx-10">
             Холбогдох {}
