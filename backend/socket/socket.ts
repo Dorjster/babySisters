5b848b646552d2595e971277dd4177598c1c685d
Dorjster <thebuyndorj@gmail.com>
2024-04-12 04:01:25 +0800

    dorjoahaid shanshu

--- backend/socket/socket.ts
+++ backend/socket/socket.ts
@@ -1,6 +1,6 @@
-import {Server as WebSocketServer} from "socket.io";
-import http from 'http';
-import express from 'express';
+import { Server as WebSocketServer } from "socket.io";
+import http from "http";
+import express from "express";
 import cors from "cors";
 
 const app = express();
@@ -9,31 +9,28 @@ app.use(cors());
 const server = http.createServer(app);
 
 const io = new WebSocketServer(server, {
-    cors: {
-        origin: "hhtp://localhost:3000",
-        methods: ["GET", "POST"]
-    }
-})
+  cors: {
+    origin: "hhtp://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
 io.on("connection", (socket) => {
-    console.log("user connected", socket.id);
+  console.log("user connected", socket.id);
 
-    socket.on("join_room", (data) => {
-        console.log(data);
-        socket.join(data); 
-    })
+  socket.on("join_room", (data) => {
+    console.log(data);
+    socket.join(data);
+  });
 
-    socket.on("send_message", (data) => {
-        console.log(data);
-        socket.to(data.room).emit("receive_message", data)
-        
-    })
-    
+  socket.on("send_message", (data) => {
+    console.log(data);
+    socket.to(data.room).emit("receive_message", data);
+  });
 });
 
 server.listen(3001, () => {
-    console.log("server is running");
-    
-})
+  console.log("server is running");
+});
 
-export {app, io, server};
+export { app, io, server };
