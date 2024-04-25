5e55bd43df293bb13b4a465662f2757bed1aada3
Abyss <160424037+Lightscythe@users.noreply.github.com>
2024-04-25 10:50:26 +0800

    Clerk Frontend
    +Middleware + Clerk + Needs Update
    +Src / App / Page + beldtsen function iin + Zasalt heregtei.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,41 @@
 import RealHome from "@/components/home/realHome";
+import Image from "next/image";
+import React from "react";
+import { useEffect } from "react";
+import axios from "axios";
+import { currentUser, auth } from "@clerk/nextjs";
+
+// env.local dotor bga production id url + /api \\
+let url: string = process.env.NEXT_PUBLIC_PRODUCTION!;
+
+// Export create account through google login. \\
 export default function Home() {
+  useEffect(() => {
+    const getAllCategories = async () => {
+      try {
+        const result = await axios.get(url);
+
+    
+
+        const { data } = await axios.post(
+          " deployed url + createbyclerk page ni link nemeerei ",
+          {
+            name: result.data?.user.name,
+            email: result.data?.user.emailAddresses[0].emailAddress,
+          }
+        );
+
+     
+        if (typeof window !== "undefined") {
+          localStorage.setItem("token", data);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getAllCategories();
+  }, []);
+
   return (
     <div className="flex-col gap-10">
       <RealHome />
--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,5 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
+
+// Ene dotor linknude orulaarai page ni signin guiger oroh clerk bhgui bvl zaaval clerk eer login hiij bj ordog bolno shu xd 
 export default authMiddleware({
   publicRoutes: ["/", "/login", "/signup", "/profile"],
 });
